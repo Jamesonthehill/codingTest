f613925d9761726ec23df08047cddeac1bf590e7
건우 <dlrjsdn53@naver.com>
2022-02-04 17:18:04 +0900

    boolean ture or false

---
+++ src/com/dormain/ch02/BooleanExample.java
@@ -0,0 +1,39 @@
+package com.dormain.ch02;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		boolean stop =true;
+		if(stop){
+			System.out.println("중지합니다.");
+			
+		} else {
+				System.out.println("시작합니다.");
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
