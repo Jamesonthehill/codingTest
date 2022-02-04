09905ccdd0d7eb006b19a547b658bcb2f9591d83
건우 <dlrjsdn53@naver.com>
2022-02-05 00:22:35 +0900

    ByteExample

---
+++ src/com/dormain/ch02/ByteExample.java
@@ -0,0 +1,35 @@
+package com.dormain.ch02;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 30;
+		byte var5 = 127;
+//		byte var6 = 128;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
