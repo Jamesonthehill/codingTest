5a719da21e2ef17bb17e87a7d415ac4e5776cecd
건우 <dlrjsdn53@naver.com>
2022-02-04 16:10:50 +0900

    if문 응용2

---
+++ src/com/dormain/ch02/VariableScopeExample.java
@@ -0,0 +1,21 @@
+package com.dormain.ch02;
+
+public class VariableScopeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			
+			int v1 = 15;
+			
+			if(v1	>	10) {
+				int v2;
+				v2 = v1 - 10;
+				int v3 = v1 + v2 + 5; // v2 변수를 아용 할수 없기 때문에 컴파일 애러뜬다.
+			}
+			
+	
+	}
+	
+
+}
