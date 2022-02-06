c827758c3ccbeb4445e1fee3fa5a1b77aa6ff592
건우 <dlrjsdn53@naver.com>
2022-02-07 00:14:05 +0900

    failurtest

---
+++ src/com/dormain/ch02/intExample.java
@@ -0,0 +1,23 @@
+package com.dormain.ch02;
+
+public class intExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+					int var1 = 10;
+					
+					// int 범위를 벗어나는 리터럴인 경우 뒤에 'L' 을 붙여줘야한다.
+					// int -2147483647 ~ 2147483647 
+				
+					int var2 = 2147483648L;
+					int var3 = 0xA;
+							
+					System.out.println(var1);
+					System.out.println(var2);
+					System.out.println(var3);
+					
+	}
+
+}
