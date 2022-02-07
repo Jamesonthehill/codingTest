25849a9646a166cf8cc0ee7ad847616e8ad51b5d
건우 <dlrjsdn53@naver.com>
2022-02-08 01:16:00 +0900

    promotionExample java

---
+++ src/com/dormain/ch02/PromotionExample.java
@@ -0,0 +1,47 @@
+package com.dormain.ch02;
+
+public class PromotionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		byte byteValue = 10;
+		System.out.println("byteValue: " + byteValue);
+		
+		
+		int intValue = byteValue;
+		System.out.println("intValue: " + intValue);
+		
+		
+		char charValue = '가';
+		System.out.println("charValue: " + charValue);
+		
+		intValue = charValue;
+		System.out.println("가의 유니코드: " + charValue );
+		
+		intValue = 500;
+		long longValue = intValue;
+		System.out.println("longValue: " + longValue);
+		
+		intValue = 200;
+		float floatValue = intValue;
+		System.out.println("intValue: " + floatValue) ;
+		
+		intValue = 200;
+		double doubleValue = intValue;
+		System.out.println("intValue: " + doubleValue);
+		
+	//	char charValue = '가';
+		
+		
+		
+		
+		
+		
+		
+		
+					
+	}
+
+}
