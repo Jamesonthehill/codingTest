1df03473224cdfdd8626f176c42e3ec888ccc425
건우 <dlrjsdn53@naver.com>
2022-02-08 01:16:13 +0900

    CastingExample.java

---
+++ src/com/dormain/ch02/CastingExample.java
@@ -0,0 +1,44 @@
+package com.dormain.ch02;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// byte < short < int < long
+		// << float < double
+		
+		
+		System.out.println("Byte.MIN_VALUE: " +  Byte.MIN_VALUE);
+		System.out.println("Byte.MAX_VALUE:" + Byte.MAX_VALUE );
+		System.out.println("Short.MIN_VALUE: " +  Short.MIN_VALUE);
+		System.out.println("Short.MAX_VALUE:" + Short.MAX_VALUE );
+		System.out.println("Integer.MIN_VALUE: " +  Integer.MIN_VALUE);
+		System.out.println("Integer.MAX_VALUE:" + Integer.MAX_VALUE );
+		System.out.println("Long.MIN_VALUE: " +  Long.MIN_VALUE);
+		System.out.println("Long.MAX_VALUE:" + Long.MAX_VALUE );
+		System.out.println("Float.MIN_VALUE: " +  Float.MIN_VALUE);
+		System.out.println("Float.MAX_VALUE:" + Float.MAX_VALUE );
+		System.out.println("Double.MIN_VALUE: " +  Double.MIN_VALUE);
+		System.out.println("Double.MAX_VALUE:" + Double.MAX_VALUE );
+		
+		
+		
+		
+		
+		/*
+		 * int intValue = 44032; System.out.println("intValue: " + intValue);
+		 * 
+		 * char charValue = (char) intValue; System.out.println(charValue);
+		 * 
+		 * long longValue = 127; intValue = (short) longValue;
+		 * System.out.println(intValue);
+		 * 
+		 * 
+		 * double doubleValue = 3.14; intValue = (int) doubleValue;
+		 * System.out.println(intValue);
+		 */
+		
+	}
+
+}
