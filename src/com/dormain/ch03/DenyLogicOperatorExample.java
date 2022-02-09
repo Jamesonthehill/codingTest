b9070473299ba82daaf8c7314a65cae372fb41af
건우 <dlrjsdn53@naver.com>
2022-02-10 01:09:47 +0900

    DenyLogicOperatorExample

---
+++ src/com/dormain/ch03/DenyLogicOperatorExample.java
@@ -0,0 +1,34 @@
+package com.dormain.ch03;
+
+public class DenyLogicOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean play = true;
+		
+		System.out.println(play); // syso + ctrl + spacebar
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
