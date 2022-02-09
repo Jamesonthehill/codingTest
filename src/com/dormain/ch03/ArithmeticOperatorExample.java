b36396330099ab546e829bd419e82aec2b6aae84
건우 <dlrjsdn53@naver.com>
2022-02-10 01:10:00 +0900

    ArithmeticOperatorExample

---
+++ src/com/dormain/ch03/ArithmeticOperatorExample.java
@@ -0,0 +1,33 @@
+package com.dormain.ch03;
+
+public class ArithmeticOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+				int v1 = 5;
+				int v2 = 2;
+				
+				int result1 = v1 + v2;
+				System.out.println("result1: " + result1);
+		
+				int result2 = v1 - v2;
+				System.out.println("result2: " + result2);
+
+				int result3 = v1 * v2;
+				System.out.println("result3: " + result3);
+
+				int result4 = v1 / v2;  // 몫만 계산
+				System.out.println("result4: " + result4);
+
+				float result5 = v1 % v2; // 나머지만 계산
+				System.out.println("result5: " + result5);
+
+				double result6 =  v1 / v2;
+				System.out.println("result6: " + result6);
+
+			
+	}
+
+}
