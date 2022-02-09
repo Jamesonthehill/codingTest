94b9972dc73912b41f85d4960dc0d5dd3c365ea5
건우 <dlrjsdn53@naver.com>
2022-02-10 01:08:54 +0900

    signOperatorExample.java

---
+++ src/com/dormain/ch03/SignOperatorExample.java
@@ -0,0 +1,27 @@
+package com.dormain.ch03;
+
+public class SignOperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			int x = -100;
+			
+			int result1 = +x;
+			
+			int result2 = -x;
+			
+			System.out.println("result1: " + result1);
+			System.out.println("result2: " + result2);
+			
+		
+		
+			short s = 100;
+	// 		short result3 = -s;
+			int result3 = -s;
+		
+			System.out.println("result3: " + result3);
+		
+	}
+
+}
