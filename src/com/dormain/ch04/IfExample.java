9b857178457b7ce271f8a175972eb8ea371c2319
건우 <dlrjsdn53@naver.com>
2022-02-11 01:35:48 +0900

    자바 대학생 학점 만들기2

---
+++ src/com/dormain/ch04/IfExample.java
@@ -0,0 +1,31 @@
+package com.dormain.ch04;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+			int score = 89;
+			
+			if(score >= 90 ) {
+				System.out.println("점수가 90 이상 큽니다.");
+				System.out.println("등급은 A 입니다.");
+			}
+				if(score < 89) 
+					System.out.println("점수가 90 미만 입니다."); // 괄호가 없으면 첫번째 줄까지만 허용해준다 ㅎㅎ
+					
+				
+				
+				
+				
+				
+				
+				
+				
+				System.out.println("등급은 B 입니다.");
+					System.out.println("등급은 B 입니다.");
+				
+			}
+	}
