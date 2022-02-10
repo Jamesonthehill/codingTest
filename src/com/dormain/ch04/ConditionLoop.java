1c8c01cf241c875effc978235d8b41bb91b48b40
건우 <dlrjsdn53@naver.com>
2022-02-11 01:36:36 +0900

    자바에대한 이론내용.

---
+++ src/com/dormain/ch04/ConditionLoop.java
@@ -0,0 +1,69 @@
+package com.dormain.ch04;
+
+public class ConditionLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		조건문
+//	
+//		 if
+//			if 만 사용하는 경우 :
+//				
+//			조건이 true/false로 떨어지는 경우
+//			if() {
+//				
+//			} else {
+//				
+//			}
+//			조건이 많은 경우
+//			if(조건식) {
+//				
+//			} else if(조건식) {
+//				
+//			} else if(조건식) {
+//				
+//			} else {
+//				
+//			}
+		
+//			번외
+//			if 안에 또는 if를 넣을 수 있을까? 무한대
+//					
+//				if() {
+//					if() {
+//						if () {
+//							
+//						}
+//					}
+//				}
+//		 switch
+//		 
+//		반복문
+//		
+//		 for
+//		 
+//		 while : 단점: 무한루프자주빠진다.
+//			 
+//		 do-while
+//			 
+//		참고
+//		 
+//		 break
+//		 
+//		 continue
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
