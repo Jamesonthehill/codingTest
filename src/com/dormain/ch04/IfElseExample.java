213694922deb7fa182e6acc0c5d8e8d1e29baf8f
건우 <dlrjsdn53@naver.com>
2022-02-11 01:36:23 +0900

    자바 if, else문 초급

---
+++ src/com/dormain/ch04/IfElseExample.java
@@ -0,0 +1,21 @@
+package com.dormain.ch04;
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			int score = 85;
+			
+			if(score >= 90) {
+				System.out.println("점수가 90보다 큽니다.");
+				System.out.println("등급은 A등급 입니다.");
+				}else{ 
+				System.out.println("등급은 90보다 작습니다.");
+				System.out.println("등급은 B등급 입니다.");
+				}
+			}
+
+		}
+	
