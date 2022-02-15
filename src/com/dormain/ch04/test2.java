e9c760901bf2a7b3ee9f58f0ee09c0c9058f4682
건우 <dlrjsdn53@naver.com>
2022-02-15 14:06:37 +0900

    로또번호만들기

---
+++ src/com/dormain/ch04/test2.java
@@ -0,0 +1,29 @@
+package com.dormain.ch04;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = (int) (Math.random() * ( 45 - 1 + 1 ) + 1 );
+		int num1 = (int) (Math.random() * ( 45 - 1 + 1 ) + 1 );
+		int num2 = (int) (Math.random() * ( 45 - 1 + 1 ) + 1 );
+		int num3 = (int) (Math.random() * ( 45 - 1 + 1 ) + 1 );
+		int num4 = (int) (Math.random() * ( 45 - 1 + 1 ) + 1 );
+		int num5 = (int) (Math.random() * ( 45 - 1 + 1 ) + 1 );
+				 
+		if (num!=num1) {
+		
+					 if (num!=num1) {
+						
+						if (num!=num2) {
+							
+							 if (num!=num3) {
+								
+								if (num!=num4) {
+									if (num!=num5) {
+								System.out.print(" " + num + " " + num1 + " " +  num2 + " " + num3 + " " + num4 + " " + num5);
+			
+									}}}}
+	}}}}
+				
