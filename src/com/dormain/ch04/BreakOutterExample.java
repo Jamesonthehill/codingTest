29b82e1b880eca1b7b19ed7d948aab9ffee22476
건우 <dlrjsdn53@naver.com>
2022-02-16 23:41:09 +0900

    BreakOutterExample

---
+++ src/com/dormain/ch04/BreakOutterExample.java
@@ -0,0 +1,20 @@
+package com.dormain.ch04;
+
+public class BreakOutterExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			Outter:
+				for(char upper='A'; upper<='Z'; upper++) {
+						for (char lower='a'; lower<='z'; lower++) {
+								System.out.println(upper + "-" + lower);
+								if(lower=='g') {
+									break Outter;
+								}
+				}
+				}
+			System.out.println("프로그램 실행종료");
+	}
+}
