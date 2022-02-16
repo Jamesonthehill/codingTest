53122fb40002a1f82b5460d5092eac8da4cf6bf4
건우 <dlrjsdn53@naver.com>
2022-02-16 23:40:54 +0900

    whileprintform1to10Example

---
+++ src/com/dormain/ch04/WhilePrintFrom1To10Example.java
@@ -0,0 +1,26 @@
+package com.dormain.ch04;
+
+public class WhilePrintFrom1To10Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int i = 1;
+			int sum = 0;
+			while(i<=10) {
+					System.out.println("합	계: " + sum);
+					
+					i++;
+					sum += i;
+				}
+					//	int sum += i;
+		
+		
+		
+		
+		
+		
+		
+	}		
+
+}
