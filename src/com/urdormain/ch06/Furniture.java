379e4baab407a65d126aaa13c396517443e81582
건우 <dlrjsdn53@naver.com>
2022-02-18 00:26:31 +0900

    가구테이블연습

---
+++ src/com/urdormain/ch06/Furniture.java
@@ -0,0 +1,13 @@
+package com.urdormain.ch06;
+
+import java.sql.Date;
+
+public class Furniture {
+	
+	String ifSeq = "1";
+	String ifName = "에이스침대";
+	String ifPrice = "289,000";
+	String ifDesc = "최초의 과학적으로 만들어진 침대이며...";
+	Integer ifDelNy = 0;
+	Date regDate;
+}
