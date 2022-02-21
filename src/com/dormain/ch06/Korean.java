6878f24fe7367df97cb8312fa4eda94b8d4660c5
건우 <dlrjsdn53@naver.com>
2022-02-22 00:17:55 +0900

    Korean, KoreanExample

---
+++ src/com/dormain/ch06/Korean.java
@@ -0,0 +1,23 @@
+package com.dormain.ch06;
+
+public class Korean {
+	
+		String nation = "대한민국";
+		String name;
+		String ssn; // ssn : social security number : 사회보안번호 : 주민등록번호
+		public Korean(String name, String ssn) {
+			
+		}
+		
+//		public Korean(String name, String ssn) {
+//
+//				name = n ;
+//				ssn  = s ;
+//		}
+
+//		public Korean(String name, String ssn) {
+//			
+//			this.name = name ;
+//			this.ssn  = ssn ;
+//		}
+}
---
+++ src/com/dormain/ch06/KoreanExample.java
@@ -0,0 +1,38 @@
+package com.dormain.ch06;
+
+public class KoreanExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Korean korean = new Korean("김일성", " 22930-90-");{
+
+			System.out.println("korean.nation : " + korean.nation);
+			System.out.println("korean.name : " + korean.name);
+			System.out.println("korean.ssn: " + korean.ssn);
+
+//		korean.name = "장원영";
+//		korean.ssn= "010234873829";
+		
+//		System.out.println("--------------");
+//		System.out.println("korean.nation : " + korean.nation);
+//		System.out.println("korean.name : " + korean.name);
+//		System.out.println("korean.ssn: " + korean.ssn);
+//
+//		Korean korean2 = new Korean("대한민국", "01022340238");
+//		
+//		System.out.println("--------------");
+//		System.out.println("korean.nation : " + korean2.nation);
+//		System.out.println("korean.name : " + korean2.name);
+//		System.out.println("korean.ssn: " + korean2.ssn);
+//		
+//		Korean korean3 = new Korean("김진짜인데요", "01022340238");
+//		
+//		System.out.println("--------------");
+//		System.out.println("korean.nation : " + korean3.nation);
+//		System.out.println("korean.name : " + korean3.name);
+//		System.out.println("korean.ssn: " + korean3.ssn);
+//			
+//	
+	}
+}
