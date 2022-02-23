83b8e0430c36dd51c240de54d754780ad5d56ca7
건우 <dlrjsdn53@naver.com>
2022-02-24 00:41:01 +0900

    Television, TelevisionExample

---
+++ src/com/dormain/ch06/Television.java
@@ -0,0 +1,13 @@
+package com.dormain.ch06;
+
+public class Television {
+
+		public String company = "Samsung";
+		public  String model = "OLED";
+		public  String info;
+		public  String desc = company + model;
+		
+//		static { // static으로 선언되지 않는 필드는 들어갈 수 없음
+//			info = company + "" + model;
+//		}
+}
---
+++ src/com/dormain/ch06/TelevisionExample.java
@@ -0,0 +1,19 @@
+package com.dormain.ch06;
+
+public class TelevisionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		System.out.println("Television.company " + Television.company);	
+//		System.out.println("Television.company " + Television.model);	
+//		System.out.println("Television.company " + Television.info);	
+		
+		Television television = new Television ();
+		System.out.println("Television.company " + television.company);	
+		System.out.println("Television.company " + television.model);	
+		System.out.println("Television.company " + television.info);	
+		
+	}
+
+}
