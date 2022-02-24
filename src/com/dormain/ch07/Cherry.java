b35d850d0e87b34770597b33d1b6019865a2b32d
건우 <dlrjsdn53@naver.com>
2022-02-25 00:13:01 +0900

    Cherry

---
+++ src/com/dormain/ch07/Cherry.java
@@ -0,0 +1,17 @@
+package com.dormain.ch07;
+
+public class Cherry extends Banana {
+
+	
+	private String strCherry = "strCherry";
+	
+	public void printCherry() {
+		System.out.println("Cherry");
+	}
+	public String getStrCherry() {
+		return strCherry;
+	}
+	public void setStrCherry(String strCherry) {
+		this.strCherry = strCherry;
+	}
+}
