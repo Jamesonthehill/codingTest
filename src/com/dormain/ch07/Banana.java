0e934c0ba399c7803f66c8e4105b666e4aed2673
건우 <dlrjsdn53@naver.com>
2022-02-25 00:12:51 +0900

    Banana

---
+++ src/com/dormain/ch07/Banana.java
@@ -0,0 +1,16 @@
+package com.dormain.ch07;
+
+public class Banana extends Apple{
+	
+		private String strBanana ="strBanana";
+
+		public void printBanana() {
+			System.out.println("Banana");
+		}
+		public String getStrBanana() {
+			return strBanana;
+		}
+		public void setStrBanana(String strBanana) {
+			this.strBanana = strBanana;
+		}
+}
