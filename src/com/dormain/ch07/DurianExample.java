6435c1ed90fa97fa7d445925953215889eb7fde8
건우 <dlrjsdn53@naver.com>
2022-02-25 00:13:16 +0900

    DurianExampleJava

---
+++ src/com/dormain/ch07/DurianExample.java
@@ -0,0 +1,22 @@
+package com.dormain.ch07;
+
+public class DurianExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				Durian durian = new Durian();
+				
+				durian.printApple();
+				durian.printBanana();
+				durian.printCherry();
+				durian.printDurian();
+				
+				System.out.println(durian.getStrApple());
+				System.out.println(durian.getStrBanana());
+				System.out.println(durian.getStrCherry());
+				System.out.println(durian.getStrDurian());
+				
+	}
+
+}
