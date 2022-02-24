8104060ed0d5b94d13d318588ea138b59c462358
건우 <dlrjsdn53@naver.com>
2022-02-25 00:12:42 +0900

    Apple

---
+++ src/com/dormain/ch07/Apple.java
@@ -0,0 +1,16 @@
+package com.dormain.ch07;
+
+public class Apple {
+ 
+		private String strApple = "strApple";
+		
+		public void printApple() {
+			System.out.println("Apple");
+		}
+		public String getStrApple() {
+			return strApple;
+		}
+		public void setStrApple(String strApple) {
+			this.strApple = strApple;
+		}
+}
