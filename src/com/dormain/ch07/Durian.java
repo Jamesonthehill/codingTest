6bd5e8c4339657f7d52fff4fe37df301a155dcb4
건우 <dlrjsdn53@naver.com>
2022-02-25 00:13:07 +0900

    Durian

---
+++ src/com/dormain/ch07/Durian.java
@@ -0,0 +1,16 @@
+package com.dormain.ch07;
+
+public class Durian extends Cherry {
+
+	private String strDurian = "strDurian";
+	
+	public void printDurian() {
+		System.out.println("Durian");
+	}
+	public String getStrDurian() {
+		return strDurian;
+	}
+	public void setStrDurian(String strDurian) {
+		this.strDurian = strDurian;
+	}
+}
