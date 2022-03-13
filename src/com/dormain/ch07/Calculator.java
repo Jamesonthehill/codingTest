efa17d04b275204bfb1a889f334b1c5d9b30ca61
건우 <dlrjsdn53@naver.com>
2022-03-13 21:33:14 +0900

    Calculaotr, Car

---
+++ src/com/dormain/ch07/Calculator.java
@@ -0,0 +1,11 @@
+package com.dormain.ch07;
+
+public class Calculator {
+
+	public double areaCircle(double r) {
+			
+			System.out.println("Calculator 객체의 areaCircle 실행" );
+				
+			return 3.14159 * r * r;
+	}
+}
