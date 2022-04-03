d3ce9285c85b57c7541b1cd67a2dcce54cbb9974
건우 <dlrjsdn53@naver.com>
2022-04-03 11:02:48 +0900

    codeing test

---
+++ src/codingtest/first.java
@@ -0,0 +1,11 @@
+package codingtest;
+
+public class first {
+		public static void main(String[] args) {
+			
+			
+	int A = 1; 	int B = 2; int C = A+B;
+	System.out.println("A + B = " + C);
+		
+		}
+}
