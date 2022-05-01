1e86436cb14a8ec3d4d5bb68dee655addc766986
james <dlrjsdn53@naver.com>
2022-05-01 19:26:39 +0900

    코딩테스트

---
+++ src/codingtest/test2.java
@@ -0,0 +1,26 @@
+
+package codingtest;
+
+		class A{
+			A(String s){
+				this.s = s + "A";
+			}
+			public fn(String s) {
+				System.out.println(this.s + s);
+			}
+			
+			class B extends A {
+				private String s;
+				B(String s){
+					this.s = s + "B";
+				}
+				public fn(String s) {
+					System.out.println(this.s + s);
+				}
+			}
+			public static void main(String[] args) {
+				A a = new B("Hello");
+				a.fn("Hi");
+			}
+		
+}
