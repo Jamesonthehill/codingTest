7a8c34b1f7226af99ef9cd129d9288ace1553ddc
james <dlrjsdn53@naver.com>
2022-05-08 21:14:48 +0900

    2022.05.08 codingtest

---
+++ src/codingtest/test5.java
@@ -0,0 +1,28 @@
+
+package codingtest;
+
+class SuperObj {
+	public void show() {
+		print();
+	}
+	public void print() {
+		print();
+		System.out.println("Super");
+	}
+}
+
+class SubObj extends SuperObj {
+	public void show() {
+		super.print();
+	}
+	public void print() {
+		System.out.print("Sub");
+	}
+}
+
+public class Soojebi {
+	public static void main(String[] args) {
+		SuperObj s = new SubObj();
+		s.show();
+	}
+}
