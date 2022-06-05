667b3c2b21c52c902478dedfea1b9806be19d604
james <dlrjsdn53@naver.com>
2022-06-05 18:10:40 +0900

    asdsa

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+module study_java {
+	exports com.dormain.ch07;
+	exports codingtest;
+	exports com.dormain.ch06;
+	exports com.dormain.ch05;
+	exports com.urdormain.ch06.exc;
+	exports com.dormain.ch04;
+	exports com.dormain.ch03;
+	exports com.dormain.ch02;
+	exports com.dormain.ch08;
+
+	requires java.sql;
+}
