58593e7aac3822f298bf058c7fe156d73f68075a
james <dlrjsdn53@naver.com>
2022-07-01 23:16:02 +0900

    A/B 문제

---
+++ src/codingtest/Main.java
@@ -0,0 +1,24 @@
+package codingtest;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+        
+        for(int A=0; A<10; A++){
+
+    	A = in.nextInt();
+
+        for(int B=0; B<10; B++) {
+
+    	B = in.nextInt();
+    	System.out.println(A/B);
+        }
+        }
+
+             
+    	}
+    }
