21c586a9f0f52a1a2f989b24a78cccfd35f668f8
James <dlrjsdn53@naver.com>
2022-09-12 04:50:46 +0900

    사칙연산 1008번 문제 답

---
+++ codingTest/src/codingTest/Main.java
@@ -0,0 +1,20 @@
+package codingTest;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+        int a;
+        int b;
+        
+        a = in.nextInt();
+        b = in.nextInt();
+        
+        System.out.println(a/b);
+        
+        
+        }  
+}
+
