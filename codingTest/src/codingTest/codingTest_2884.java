0c050613cfd4b9794cbdd263e88a64b2d5441982
James <dlrjsdn53@naver.com>
2022-09-28 23:59:46 +0900

    백준 2884번

---
+++ codingTest/src/codingTest/codingTest_2884.java
@@ -0,0 +1,33 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class codingTest_2884 {
+	
+	
+	
+	public static void main (String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int h = in.nextInt();
+		int m = in.nextInt();
+		in.close();
+		
+		if(m<45) {
+			h--;
+			m = 60 + (m - 45);
+			
+			if ( h < 0 ) {
+				h = 23;
+				
+			}
+				System.out.println(h+" "+m);
+			}  
+			
+				else if(m>=45)  {
+				m -= 45;
+				System.out.println(h+" "+m);
+			}
+	}
+}
