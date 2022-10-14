33b547d1cad60ceef9e84c3edba761d89210c267
geotwo <geotwo@DESKTOP-78TD05T>
2022-10-14 14:04:00 +0900

    a

---
+++ dj_water/src/dj_water_water/water25304.java
@@ -0,0 +1,31 @@
+package dj_water_water;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class water25304 {
+	
+	public static void main (String[] args) throws Exception{
+	
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	StringTokenizer st;
+
+	int A = Integer.parseInt(br.readLine());
+	int B = Integer.parseInt(br.readLine());
+	int C;
+	int F = 0;
+	for(int i=0; i<B; i++) {
+		st = new StringTokenizer(br.readLine());
+		C = (Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken()));
+		F += C;
+	}
+	System.out.println(F);
+	if(A==F) {
+		System.out.println("Yes");
+	}	else {
+		System.out.println("No");
+	}
+	}
+}
