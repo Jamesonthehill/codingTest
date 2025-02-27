677353559b9744fee2eecbd0e37f484b245e7dc2
James <dlrjsdn53@naver.com>
2025-02-27 16:06:17 +0900

    Greatest Common Divisor of Strings

--- codingTest/src/Leetcode/Leetcode2.java
+++ codingTest/src/Leetcode/Leetcode2.java
@@ -1,8 +1,9 @@
 
-Package Leetcode
 
 class Leetcode2 {
     public static void main(String[] args){
         System.out.println("========== Greatest Common Divisor of Strings");
+        System.out.println();
+    
     }
 }
