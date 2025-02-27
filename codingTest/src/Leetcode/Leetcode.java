8bafb587727705bb8d8e94f88c90b832653a355c
James <dlrjsdn53@naver.com>
2025-02-27 14:56:24 +0900

    leetcode - Merge Strings Alternately

--- codingTest/src/Leetcode/Leetcode.java
+++ codingTest/src/Leetcode/Leetcode.java
@@ -22,45 +22,27 @@
 package Leetcode;
 
 class Leetcode {  // ✅ 클래스명이 파일명과 동일해야 함
-    public static String main(String word1, String word2) {
+    public String Leetcode(String word1, String word2) {
         System.out.println("========== Merge Strings Alternately  ");
         StringBuilder mergeString = new StringBuilder();
-        Environment environment = new Environment();
-        int i = 0; int j = 0;
+        int i = 0;
+        int j = 0;
 
         while (i < word1.length() && j < word2.length()) {
-            environment.Setting();
             mergeString.append(word1.charAt(i++));
             mergeString.append(word2.charAt(j++));
         }
 
-        while(i < word1.length()){
-            environment.Setting2();
+        while (i < word1.length()) {
+            mergeString.append(word2.charAt(j++));
             mergeString.append(word1.charAt(i++));
         }
-        while(j < word2.length()){
-            environment.Setting3();
+
+        while (j < word2.length()) {
+            mergeString.append(word1.charAt(i++));
             mergeString.append(word2.charAt(j++));
         }
 
         return mergeString.toString();
-        }
-
-     static class Environment {
-        static void Setting() {
-
-            String word1 = "abc";
-            String word2 = "pqr";
-
-        }
-        static void Setting2() {
-
-            String word1 =  "ab";
-            String word2 =  "pqer";
-        }
-        static void Setting3() {
-            String word1 = "abcd";
-            String word2 = "pq";
-        }
-      }
     }
+}
