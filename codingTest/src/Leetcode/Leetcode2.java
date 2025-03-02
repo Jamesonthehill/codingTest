ba01cc15483bfb2442c67114b64501c321173a3d
James <dlrjsdn53@naver.com>
2025-03-02 15:17:48 +0900

    with prof.Methew

--- codingTest/src/Leetcode/Leetcode2.java
+++ codingTest/src/Leetcode/Leetcode2.java
@@ -1,9 +1,32 @@
-
+import java.util.ArrayList;
 
 class Leetcode2 {
     public static void main(String[] args){
-        System.out.println("========== Greatest Common Divisor of Strings");
-        System.out.println();
+
+        System.out.println("========== start program");
+
+        ArrayList<Integer> list = new ArrayList<Integer>() {{
+            add(1);
+            add(2);
+            add(3);
+            add(4);
+            add(5);
+        }};
+
+        int i = 0;
+
+        while ( i < list.size()){
+            
+            if (  list.get(i) == 2 ){
+                System.out.println(list);
+            } else if ( list.get(i) == 4) {
+                System.out.println(list);
+            } else {
+                System.out.println("there is no answer");
+            }
+            i++;
+        }
     
+
     }
 }
