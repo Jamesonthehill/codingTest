e6d8ff2eb9e2ccd4eb31c30242900e29f4a8a944
James <dlrjsdn53@naver.com>
2025-03-02 17:57:50 +0900

    Code Testcase Test Result Test Result 1431. Kids With the Greatest Number of Candies

---
+++ codingTest/src/Leetcode/Candies.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Candies  {
+    public static void main (String[] args) throws NullPointerException {
+
+        System.out.println("========== Start program");
+            
+            HashSet<Integer> list = new HashSet<Integer>(Arrays.asList(2,3,5,3,3));
+            
+            int i = 0;
+            System.out.println(list);
+            
+            while ( i < list.size()) {
+                
+            }
+
+    }
+}
