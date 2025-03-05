959e7db02d263560aca3974bd927b822b62dd3f0
James <dlrjsdn53@naver.com>
2025-03-05 13:12:31 +0900

    Candies

--- codingTest/src/Leetcode/Candies.java
+++ codingTest/src/Leetcode/Candies.java
@@ -1,20 +1,29 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class Candies  {
     public static void main (String[] args) throws NullPointerException {
 
         System.out.println("========== Start program");
             
-            HashSet<Integer> list = new HashSet<Integer>(Arrays.asList(2,3,5,3,3));
-            
-            int i = 0;
+            ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(2,3,5,1,3));
+            ArrayList<Boolean> list2 = new ArrayList<>();
+            int pastResult = 0;
+
             System.out.println(list);
             
-            while ( i < list.size()) {
-                
-            }
+        for ( int i = 0;  i < list.size(); i++ ){
 
+            int R = 3;
+            int J = list.get(i) + R;
+            
+            if ( (pastResult > J  ) ) {
+                list2.add(true);
+            } else {
+                list2.add(false);
+            }
+            pastResult = list.get(i); // this part, you can take the result of prior one.
+        }       
+        System.out.println(list2);
     }
 }
