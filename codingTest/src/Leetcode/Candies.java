a64594bebee992e0423666ddc9f15abeb6f24006
James <dlrjsdn53@naver.com>
2025-03-06 17:31:43 +0900

    Kids With the Greatest Number of Candies - Answer

--- codingTest/src/Leetcode/Candies.java
+++ codingTest/src/Leetcode/Candies.java
@@ -1,29 +1,20 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
 
-public class Candies  {
-    public static void main (String[] args) throws NullPointerException {
 
-        System.out.println("========== Start program");
-            
-            ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(2,3,5,1,3));
-            ArrayList<Boolean> list2 = new ArrayList<>();
-            int pastResult = 0;
+        System.out.println("========== Candies problem")
+        int maxCandies = 0;
+        for (int candy : candies){
+            maxCandies = Math.max(candy, maxCandies);
+        }
 
-            System.out.println(list);
-            
-        for ( int i = 0;  i < list.size(); i++ ){
+        ArrayList<Boolean> list = new ArrayList<Boolean>();
+        for (int candy : candies){
+            list.add(candy + extraCandies >= maxCandies);
+        }
 
-            int R = 3;
-            int J = list.get(i) + R;
-            
-            if ( (pastResult > J  ) ) {
-                list2.add(true);
-            } else {
-                list2.add(false);
-            }
-            pastResult = list.get(i); // this part, you can take the result of prior one.
-        }       
-        System.out.println(list2);
+
+        return list;
     }
 }
