d7d7c95bb09ddfc5b76103c4d6e619596e1c8dda
James <dlrjsdn53@naver.com>
2025-03-16 00:52:08 +0900

    dataset

---
+++ codingTest/src/Leetcode/Assistance.java
@@ -0,0 +1,18 @@
+ 
+ 
+ 
+ class Dataset {
+    // Constructor of Dataset
+    public Dataset() {
+        System.out.println("Dataset() is working...");
+    }
+}
+
+public class Assistance {
+    public static void main(String[] args) {
+        System.out.println("program start");
+
+        // Create an instance of Dataset
+//        System.out.println(Dataset());
+    }
+}
